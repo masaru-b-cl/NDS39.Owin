17620490b25c161b952489a62b82415b951380b8
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2014-12-05 23:14:29 +0900

    startup "Hello world"

---
+++ NDS39.Owin.Web/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(NDS39.Owin.Web.Startup))]
+
+namespace NDS39.Owin.Web
+{
+  public class Startup
+  {
+    public void Configuration(IAppBuilder app)
+    {
+      app.Run(async context =>
+        {
+          var response = context.Response;
+          response.StatusCode = 200;
+          response.ContentType = "text/plain";
+          await response.WriteAsync("Hello world");
+        });
+    }
+  }
+}
