40094835fb2f90d1baf478aad4fac883bc946ffc
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2014-12-05 23:18:52 +0900

    add self-host app

---
+++ NDS39.Owin.SelfHost/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+using Microsoft.Owin.Hosting;
+
+using NDS39.Owin.App;
+
+namespace NDS39.Owin.SelfHost
+{
+  class Program
+  {
+    private static ManualResetEvent quitEvent = new ManualResetEvent(false);
+
+    static void Main(string[] args)
+    {
+      Console.CancelKeyPress += (sender, e) =>
+      {
+        quitEvent.Set();
+        e.Cancel = true;
+      };
+
+      var port = 9000;
+      if (args.Any())
+      {
+        int.TryParse(args[0], out port);
+      }
+
+      var url = String.Format("http://+:{0}", port);
+      using (WebApp.Start<Startup>(url))
+      {
+        Console.WriteLine("Started");
+        quitEvent.WaitOne();
+      }
+    }
+  }
+}
